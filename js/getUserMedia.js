a1ac402659eb538f2540d5cff4a843afbab05436
mjiid <habush1610@gmail.com>
2024-04-19 21:46:54 +0100

    Capture of local peer's audio and video streams

---
+++ js/getUserMedia.js
@@ -0,0 +1,27 @@
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+var constraints = {
+  audio: false,
+  video: {
+    width: { min: 640, ideal: 1280, max: 1920 },
+    height: { min: 480, ideal: 720, max: 1080 }
+  }
+};
+
+var video = document.querySelector("video");
+
+function successCallback(stream) {
+  window.stream = stream;
+  if (window.URL) {
+    video.srcObject = stream;
+  } else {
+    video.src = stream;
+  }
+  video.play();
+}
+
+function errorCallback(error) {
+  console.log("navigator.getUserMedia error: ", error);
+}
+
+navigator.getUserMedia(constraints, successCallback, errorCallback);
